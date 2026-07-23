5357d793a780026e3bcb30a6fe22c13ffe21e3bd
Dail Flores <dflores2t@gmail.com>
2026-07-23 16:56:29 -0600

    [API] Remove api/ from git tracking (deployed separately)

--- api/blog/get.php
+++
@@ -1,8 +0,0 @@
-<?php
-declare(strict_types=1);
-require_once __DIR__ . '/../bootstrap.php';
-
-use App\Controllers\BlogController;
-
-$controller = new BlogController();
-$controller->get();
--- api/projects/get.php
+++
@@ -1,8 +0,0 @@
-<?php
-declare(strict_types=1);
-require_once __DIR__ . '/../bootstrap.php';
-
-use App\Controllers\ProjectController;
-
-$controller = new ProjectController();
-$controller->get();
