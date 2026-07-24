a4189bf84af329609322d3b7c891bd205c08b62a
Dail Flores <dflores2t@gmail.com>
2026-07-24 09:56:44 -0600

    [FIX] GSC cleanup: add Netlify redirects and remove dead blog slugs
    - Create public/_redirects with /whatsapp and /wcanal 301 redirects
    - Remove 4 dead blog post entries from blog-slugs-map.js
      (typescript-avanzado, vuejs-composition-api, astro-static-site, nodejs-backend-apis)

--- src/i18n/blog-slugs-map.js
+++ src/i18n/blog-slugs-map.js
@@ -1,13 +1,5 @@
 /** @type {Record<string, string>} */
-export const blogSlugToEn = {
-  'typescript-avanzado': 'typescript-advanced-en',
-  'vuejs-composition-api': 'vuejs-composition-api-en',
-  'astro-static-site': 'astro-static-site-en',
-  'nodejs-backend-apis': 'nodejs-backend-apis-en',
-};
+export const blogSlugToEn = {};
 
 /** @type {Record<string, string>} */
 export const blogSlugToEs = {};
-for (const [es, en] of Object.entries(blogSlugToEn)) {
-  blogSlugToEs[en] = es;
-}
